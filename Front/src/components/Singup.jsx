339ec130faf23e886375336285ba373ee4c263ce
Khaleddamascene <khaled-marai1@hotmail.com>
2024-11-14 13:10:36 +0200

    rekisteroidy valmis

--- Front/src/components/Singup.jsx
+++ Front/src/components/Singup.jsx
@@ -1,83 +1,110 @@
-import React, { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
+function Signup() {    
 
-const Signup = () => {
-    // const { Rekisteroidy } = useContext(App_Context);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate('');
-
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     axios.post("http://localhost:3000/api/Rekisteroidy", { name, email, password })
-    //         .then(result => {
-    //             console.log(result);
-    //             navigate("/login");
-    //         })
-    //         .catch(err => console.log(err));
-    // }
-
+    const [nimi, setNimi] = useState()
+    const [sahkoposti, setSahkoposti] = useState()
+    const [salasana, setSalasana] = useState()
+    const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
-
+        axios.post("http://localhost:3000/api/Rekisteroidy", { nimi, sahkoposti, salasana })
+        .then(result => {console.log(result)
+    navigate("/login")
+        })
+        .catch(err => console.log(err))
     }
 
-    return (
-        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-            <div className="bg-white p-3 rounded w-25">
-                <h2><center>Sign Up</center></h2>
 
-                <form>
-                    <div className="mb-3">
-                        <label htmlFor="name">
-                            <strong>Name</strong>
-                        </label>
-                        <input type="text"
-                            placeholder='Enter Name'
-                            autoComplete='off'
-                            name='name'
-                            className='form-control rounded-0'
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email">
-                            <strong>Email</strong>
-                        </label>
-                        <input type="email"
-                            placeholder='Enter Email'
-                            autoComplete='off'
-                            name='email'
-                            className='form-control rounded-0'
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password">
-                            <strong>Password</strong>
-                        </label>
-                        <input type="password"
-                            placeholder='Enter Password'
-                            name='password'
-                            className='form-control rounded-0'
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button type="submit" className="btn btn-success w-100 rounded-0">
-                        Sign Up
-                    </button>
+return (
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+        <div className="bg-white p-3 rounded w-25">
+        <h2><center>Rekisteroidy</center></h2>
+
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Nimi</strong>
+                    </label>
+                    <input type="text" 
+                    placeholder='Syötä nimi' 
+                    autoComplete='off' 
+                    name='email' 
+                    className='form-control rounded-0'
+                    onChange={(e) => setNimi(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Sähköposti</strong>
+                    </label>
+                    <input type="text" 
+                    placeholder='Syötä sähköposti' 
+                    autoComplete='off' 
+                    name='email' 
+                    className='form-control rounded-0' 
+                    onChange={(e) => setSahkoposti(e.target.value)}
+
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                        <strong>Salasana</strong>
+                    </label>
+                    <input type="password" 
+                    placeholder='Syötä salasana' 
+                    name='password' 
+                    className='form-control rounded-0' 
+                    onChange={(e) => setSalasana(e.target.value)}
+
+                    />
+                </div>
+                <button type="submit" className="btn btn-success w-100 rounded-0">
+                Rekisteroidy
+                </button>
                 </form>
                 <p>Already have an account?</p>
                 <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
                     Login
                 </Link>
-            </div>
+            
         </div>
-    );
+    </div>
+);
 }
 
 export default Signup;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
