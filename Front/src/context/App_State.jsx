5b6ed3762c482dc47c3489f8d70c31ea75a88b55
Ak0773 <120209368+Ak0773@users.noreply.github.com>
2024-11-19 11:57:16 +0200

    tuotevalmis

--- Front/src/context/App_State.jsx
+++ Front/src/context/App_State.jsx
@@ -1,121 +1,98 @@
-import React, { useEffect, } from 'react';
-import { useState } from 'react';
-import axios from 'axios';
-import { AppContext } from './App_Context';
-
-
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { AppContext } from './App_Context'
 
 const App_State = ({ children }) => {
+	let url = 'http://localhost:3000/api'
+	const [tuotteet, setTuotteet] = useState([])
 
+	useEffect(() => {
+		const fetchTuote = async () => {
+			try {
+				const response = await axios.get(`${url}/`, {
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					withCredentials: true,
+				})
+				setTuotteet(response.data.tuotteet)
+			} catch (err) {
+				console.error('Virhe haettaessa tuotteita:', err)
+			}
+		}
+		fetchTuote()
+	}, [])
 
+	//*Rekisteroidy
+	const Rekisteroidy = async (nimi, sahkoposti, salasana) => {
+		const api = await axios.post(
+			`${url}/Rekisteroidy`,
+			{
+				nimi,
+				sahkoposti,
+				salasana,
+			},
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				withCredentials: true,
+			}
+		)
+		return api
+	}
 
+	//* kirjaudu
+	const Kirjaudu = async (sahkoposti, salasana) => {
+		const api = await axios.post(
+			`${url}/kirjaudu`,
+			{
+				sahkoposti,
+				salasana,
+			},
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				withCredentials: true,
+			}
+		)
+		return api
+	}
 
+	//* tee Tuote
+	const teeTuote = async (tuoteNimi, hinta, tiedot, kuva) => {
+		const api = await axios.post(
+			`${url}/teeTuote`,
+			{
+				tuoteNimi,
+				hinta,
+				tiedot,
+				kuva,
+			},
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				withCredentials: true,
+			}
+		)
+		return api
+	}
 
-  let url = 'http://localhost:3000/api';
-  const [tuotteet, setTuotteet] = useState([])
+	//* löydä tuote id avulla
+	const tuoteId = async id => {
+		const api = await axios.get(`${url}/${id}`, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			withCredentials: true,
+		})
+		return api
+	}
 
-
-
-  useEffect(() => {
-    const fetchTuote = async () => {
-      try {
-        const response = await axios.get(`${url}/`,
-          {
-            headers: {
-              "Content-Type": "application/json"
-            },  
-            withCredentials: true
-          });
-        setTuotteet(response.data.tuotteet);
-      }
-      catch (err) {
-        console.error("Virhe haettaessa tuotteita:", err);
-      }
-    };
-    fetchTuote()
-
-  }, []);
-
-
-  //*Rekisteroidy
-  const Rekisteroidy = async (nimi, sahkoposti, salasana) => {
-    const api = await axios.post(`${url}/Rekisteroidy`,
-      {
-        nimi,
-        sahkoposti,
-        salasana
-      },
-      {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        withCredentials: true
-      });
-    return api
-  }
-
-
-  //* kirjaudu
-  const Kirjaudu = async (sahkoposti, salasana) => {
-    const api = await axios.post(`${url}/kirjaudu`,
-      {
-        sahkoposti,
-        salasana
-      },
-      {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        withCredentials: true
-      });
-    return api
-  }
-
-  //* tee Tuote
-  const teeTuote = async (tuoteNimi, hinta, tiedot, kuva) => {
-    const api = await axios.post(`${url}/teeTuote`, {
-      tuoteNimi,
-      hinta,
-      tiedot,
-      kuva  
-    },
-      {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        withCredentials: true
-      })
-    return api
-
-  }
-
-  //* löydä tuote id avulla 
-  const tuoteId = async (id) => {
-    const api = await axios.get(`${url}/${id}`,
-      {
-        headers:{
-            "Content-Type": "application/json"
-        },
-        withCredentials: true
-      })
-      return api
-  }
-
-  return (
-
-
-  
-
-
-
-    <AppContext.Provider value={Rekisteroidy}>
-
-
-
-      {children}
-      
-    </AppContext.Provider>
-  );
+	return <AppContext.Provider value={teeTuote}>{children}</AppContext.Provider>
 }
 
-export default App_State;
+export default App_State
