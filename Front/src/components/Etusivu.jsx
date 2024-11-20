45afffa5e898b82dca6b6906f23bf83b7f56e1e5
Ak0773 <120209368+Ak0773@users.noreply.github.com>
2024-11-20 09:56:58 +0200

    ei mit uutta

--- Front/src/components/Etusivu.jsx
+++ Front/src/components/Etusivu.jsx
@@ -1,17 +1,33 @@
-import React, { useContext } from 'react'
-import { AppContext } from '../context/App_Context';
-
-
+import React, { useState, useEffect } from 'react'
+import '../img/kuva1.jpg'
+import { AppContext } from '../context/App_Context'
 
 const Etusivu = () => {
+	const images = [
+		'kuva1.jpg', // Vaihda tiedostonimiä omiin kuviisi
+		'kuva2.jpg',
+		'kuva3.jpg',
+	]
 
+	const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentImageIndex(prevIndex => (prevIndex + 1) % images.length)
+		}, 5000) // 5000ms = 5 sekuntia
 
-  return (
-    <div>
-      <h3>Tämä on Etusivu</h3>
-    </div>
-  )
+		return () => clearInterval(interval) // Siivotaan interval komponentin poistuessa
+	}, [images.length])
+
+	return (
+		<div>
+			<img
+				src={images[currentImageIndex]}
+				alt={`Slide ${currentImageIndex + 1}`}
+				style={{ width: '100%', height: 'auto' }} // Mukauta tarpeen mukaan
+			/>
+		</div>
+	)
 }
 
 export default Etusivu
