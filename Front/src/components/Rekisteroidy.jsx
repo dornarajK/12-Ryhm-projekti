2a8ca9ddc18e22a58fe7866989f133493f390e58
Khaleddamascene <khaled-marai1@hotmail.com>
2024-12-05 10:04:26 +0200

    .

--- Front/src/components/Rekisteroidy.jsx
+++ Front/src/components/Rekisteroidy.jsx
@@ -27,15 +27,14 @@ function Rekisteroidy() {
 		}
 	}
 
-<<<<<<< HEAD
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post("http://localhost:3000/api/Rekisteroidy", { nimi, sahkoposti, salasana })
-        .then(result => {console.log(result)
-    navigate("/kirjaudu")
-        })
-        .catch(err => console.log(err))
-    }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     axios.post("http://localhost:3000/api/Rekisteroidy", { nimi, sahkoposti, salasana })
+    //     .then(result => {console.log(result)
+    // navigate("/kirjaudu")
+    //     })
+    //     .catch(err => console.log(err))
+    // }
 
 
 
@@ -94,64 +93,6 @@ return (
         </div>
     </div>
 );
-=======
-	return (
-		<div className='rek'>
-			<div className='rek'>
-				<h2 className='center'>Rekisteröidy</h2>
-				<form onSubmit={handleSubmit}>
-					<div className='mb-3'>
-						<label htmlFor='nimi'>
-							<strong>Nimi</strong>
-						</label>
-						<input
-							type='text'
-							id='nimi'
-							placeholder='Syötä nimi'
-							autoComplete='off'
-							className='form-control'
-							value={nimi}
-							onChange={e => setNimi(e.target.value)}
-							required
-						/>
-					</div>
-					<div className='mb-3'>
-						<label htmlFor='sahkoposti'>
-							<strong>Sähköposti</strong>
-						</label>
-						<input
-							type='email'
-							id='sahkoposti'
-							placeholder='Syötä sähköposti'
-							autoComplete='off'
-							className='form-control'
-							value={sahkoposti}
-							onChange={e => setSahkoposti(e.target.value)}
-							required
-						/>
-					</div>
-					<div className='mb-3'>
-						<label htmlFor='salasana'>
-							<strong>Salasana</strong>
-						</label>
-						<input
-							type='password'
-							id='salasana'
-							placeholder='Syötä salasana'
-							className='form-control'
-							value={salasana}
-							onChange={e => setSalasana(e.target.value)}
-							required
-						/>
-					</div>
-					<button type='submit' className='btn btn-success w-100'>
-						Rekisteröidy
-					</button>
-				</form>
-			</div>
-		</div>
-	)
->>>>>>> main
 }
 
 export default Rekisteroidy
